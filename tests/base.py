378ba6c5d9b930469052ccc64ce59c5611257276
Amirouche Boubekki <amirouche.boubekki@gmail.com>
2013-04-14 21:40:10 +0200

    fix tests to work with create_vertex/vertex

--- tests/base.py
+++ tests/base.py
@@ -17,69 +17,77 @@ class BaseTestGraph(TestCase):
 
     def test_create_vertex(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
+            vertex = self.graph.create_vertex()
         self.assertTrue(vertex)
 
     def test_create_edge(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
-            other = self.graph.vertex()
-            edge = self.graph.edge(vertex, 'link', other)
+            vertex = self.graph.create_vertex()
+            other = self.graph.create_vertex()
+            edge = self.graph.create_edge(vertex, 'link', other)
         self.assertTrue(edge)
+
+    def test_retrieve_vertex(self):
+        with self.graph.transaction():
+            vertex = self.graph.create_vertex()
+        id = vertex.id()
+        with self.graph.transaction():
+            same = self.graph.vertex(id)
+        self.assertEqual(same.id(), id)
 
     def test_vertex_outgoings(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
-            other = self.graph.vertex()
-            edge = self.graph.edge(vertex, 'link', other)
+            vertex = self.graph.create_vertex()
+            other = self.graph.create_vertex()
+            edge = self.graph.create_edge(vertex, 'link', other)
         copy_edge = next(vertex.outgoings())
         self.assertEqual(copy_edge, edge)
 
     def test_vertex_incomings(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
-            other = self.graph.vertex()
-            edge = self.graph.edge(vertex, 'link', other)
+            vertex = self.graph.create_vertex()
+            other = self.graph.create_vertex()
+            edge = self.graph.create_edge(vertex, 'link', other)
         copy_edge = next(other.incomings())
         self.assertEqual(copy_edge, edge)
 
     def test_vertex_outgoings_with_label(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
-            other = self.graph.vertex()
-            link = self.graph.edge(vertex, 'link', other)
-            self.graph.edge(vertex, 'edge', other)
+            vertex = self.graph.create_vertex()
+            other = self.graph.create_vertex()
+            link = self.graph.create_edge(vertex, 'link', other)
+            self.graph.create_edge(vertex, 'edge', other)
         copy_link = next(vertex.outgoings('link'))
         self.assertEqual(copy_link, link)
 
     def test_vertex_incomings_with_labels(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
-            other = self.graph.vertex()
-            link = self.graph.edge(vertex, 'link', other)
-            self.graph.edge(vertex, 'edge', other)
+            vertex = self.graph.create_vertex()
+            other = self.graph.create_vertex()
+            link = self.graph.create_edge(vertex, 'link', other)
+            self.graph.create_edge(vertex, 'edge', other)
         copy_link = next(other.incomings('link'))
         self.assertEqual(copy_link, link)
 
     def test_test_get_vertex(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
+            vertex = self.graph.create_vertex()
         id = vertex.id()
-        self.assertEqual(self.graph.get_vertex(id), vertex)
+        self.assertEqual(self.graph.vertex(id), vertex)
 
     def test_test_get_edge(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
-            other = self.graph.vertex()
-            edge = self.graph.edge(vertex, 'link', other)
+            vertex = self.graph.create_vertex()
+            other = self.graph.create_vertex()
+            edge = self.graph.create_edge(vertex, 'link', other)
         id = edge.id()
-        self.assertEqual(self.graph.get_edge(id), edge)
+        self.assertEqual(self.graph.edge(id), edge)
 
     def test_iter_vertices(self):
         with self.graph.transaction():
-            self.graph.vertex()
-            self.graph.vertex()
-            self.graph.vertex()
+            self.graph.create_vertex()
+            self.graph.create_vertex()
+            self.graph.create_vertex()
         num = 0
         for vertex in self.graph.vertices():
             self.assertTrue(vertex)
@@ -88,12 +96,12 @@ class BaseTestGraph(TestCase):
 
     def test_iter_edges(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
-            other = self.graph.vertex()
-            moar = self.graph.vertex()
-            self.graph.edge(vertex, 'link', other)
-            self.graph.edge(other, 'link', moar)
-            self.graph.edge(moar, 'link', vertex)
+            vertex = self.graph.create_vertex()
+            other = self.graph.create_vertex()
+            moar = self.graph.create_vertex()
+            self.graph.create_edge(vertex, 'link', other)
+            self.graph.create_edge(other, 'link', moar)
+            self.graph.create_edge(moar, 'link', vertex)
         num = 0
         for edge in self.graph.edges():
             self.assertTrue(edge)
@@ -102,68 +110,68 @@ class BaseTestGraph(TestCase):
 
     def test_edge_label(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
-            other = self.graph.vertex()
-            edge = self.graph.edge(vertex, 'link', other)
+            vertex = self.graph.create_vertex()
+            other = self.graph.create_vertex()
+            edge = self.graph.create_edge(vertex, 'link', other)
         self.assertEqual(edge.label(), 'link')
 
     def test_edge_start(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
-            other = self.graph.vertex()
-            edge = self.graph.edge(vertex, 'link', other)
+            vertex = self.graph.create_vertex()
+            other = self.graph.create_vertex()
+            edge = self.graph.create_edge(vertex, 'link', other)
         self.assertEqual(edge.start(), vertex)
 
     def test_edge_end(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
-            other = self.graph.vertex()
-            edge = self.graph.edge(vertex, 'link', other)
+            vertex = self.graph.create_vertex()
+            other = self.graph.create_vertex()
+            edge = self.graph.create_edge(vertex, 'link', other)
         self.assertEqual(edge.end(), other)
 
     def test_set_property_string(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
+            vertex = self.graph.create_vertex()
             vertex['foo'] = 'bar'
         self.assertEqual(vertex['foo'], 'bar')
 
     def test_set_property_integer(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
+            vertex = self.graph.create_vertex()
             vertex['foo'] = 123
         self.assertEqual(vertex['foo'], 123)
 
     def test_set_property_list_of_interger(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
+            vertex = self.graph.create_vertex()
             vertex['foo'] = [1, 2, 3]
         self.assertItemsEqual(vertex['foo'], [1, 2, 3])
 
     def test_set_property_list_of_string(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
+            vertex = self.graph.create_vertex()
             vertex['foo'] = ['1', '2', '3']
         self.assertItemsEqual(vertex['foo'], ['1', '2', '3'])
 
     def test_delete_vertex(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
+            vertex = self.graph.create_vertex()
         with self.graph.transaction():
             vertex.delete()
         self.assertEqual(len(list(self.graph.vertices())), 0)
 
     def test_delete_edge(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
-            other = self.graph.vertex()
-            edge = self.graph.edge(vertex, 'link', other)
+            vertex = self.graph.create_vertex()
+            other = self.graph.create_vertex()
+            edge = self.graph.create_edge(vertex, 'link', other)
         with self.graph.transaction():
             edge.delete()
         self.assertEqual(len(list(self.graph.edges())), 0)
 
     def test_keys(self):
         with self.graph.transaction():
-            vertex = self.graph.vertex()
+            vertex = self.graph.create_vertex()
             vertex['foo'] = 'bar'
             vertex['baz'] = 'egg'
         self.assertItemsEqual(vertex.keys(), ['foo', 'baz'])
@@ -179,13 +187,13 @@ class BaseTestGraph(TestCase):
     def test_index_put(self):
         index = self.graph.index.create('foo', self.graph.VERTEX)
         with self.graph.transaction():
-            vertex = self.graph.vertex()
+            vertex = self.graph.create_vertex()
         index.put('key', 'value', vertex)
 
     def test_index_retrieve(self):
         index = self.graph.index.create('foo', self.graph.VERTEX)
         with self.graph.transaction():
-            vertex = self.graph.vertex()
+            vertex = self.graph.create_vertex()
         index.put('key', 'value', vertex)
         vertex_copy = next(index.get('key', 'value'))
         self.assertEqual(vertex, vertex_copy)
